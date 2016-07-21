538f56cc66cf698a26ae3d05aea0b08c4c29c23a
Raffles Gun <Raffles.Gun@nintex.com>
2016-07-21 06:18:29 -0700

    Updated ITaskItem test data

--- src/webparts/myAssignedTasks/tests/MockHttpClient.ts
+++ src/webparts/myAssignedTasks/tests/MockHttpClient.ts
@@ -3,8 +3,8 @@ import { ITaskItem } from '../components/TaskItem';
 export default class MockHttpClient {
 
   private static _items: ITaskItem[] = [
-    { Title: 'Leave Approval', Id: '1', Description: 'Leave approval', Priority: 2, DueDate: null },
-    { Title: 'Expenses Approval', Id: '2', Description: 'Expenses approval', Priority: 2, DueDate: new Date('2016-07-22T21:13:49Z') }
+    { Title: 'Leave Approval', Id: 1, Description: 'Leave approval', Priority: 2, DueDate: null },
+    { Title: 'Expenses Approval', Id: 2, Description: 'Expenses approval', Priority: 2, DueDate: new Date('2016-07-22T21:13:49Z') }
   ];
 
   public static get(restUrl: string, options?: any): Promise<ITaskItem[]> {
